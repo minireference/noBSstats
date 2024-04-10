0c9e704cf9b290a16082550a38f5aecbb07e9f50
Ivan Savov <ivan.savov@gmail.com>
2024-04-10 11:09:04 -0400

    Improve scripts/process_tutorials.py to handle attachments/ images

--- scripts/process_tutorials.py
+++ scripts/process_tutorials.py
@@ -33,14 +33,17 @@ Processing steps:
 # from process_notebooks.py
 """
 from copy import deepcopy
+import glob
 import nbformat
 import os
+import re
 
 
 PROJECT_DIR = "/Users/ivan/Projects/Minireference/STATSbook/noBSstatsnotebooks/"
 SOLUTIONS_DIR = "tutorials/solutions/python/"
 SOLUTIONS_PATH = os.path.join(PROJECT_DIR, SOLUTIONS_DIR)
-GITHUB_TREE_URL = "https://raw.githubusercontent.com/minireference/noBSstats/main" 
+GITHUB_TREE_URL = "https://github.com/minireference/noBSstats/blob/main"
+GITHUB_RAW_TREE_URL = "https://raw.githubusercontent.com/minireference/noBSstats/main"
 
 def has_solution(cell):
     """Return True if cell is marked as containing an exercise solution."""
@@ -52,6 +55,24 @@ def has_solution(cell):
     )
 
 
+def clear_solutions_dir():
+    solutions_dir = SOLUTIONS_PATH
+    solutions_py_pat = os.path.join(solutions_dir, '*.py')
+    solutions_py_files = glob.glob(solutions_py_pat)
+    for solutions_py_file in solutions_py_files:
+        os.remove(solutions_py_file)
+
+
+def rewrite_attachment_links(cell):
+    tut_src_att_url = GITHUB_RAW_TREE_URL + "/tutorials/src/attachments/"
+    if cell.cell_type == 'markdown':
+        cell_source = cell["source"]
+        attachments_pat = r'(?:\./)?attachments/'
+        if re.search(attachments_pat, cell_source):
+            updated_source = re.sub(attachments_pat, tut_src_att_url, cell_source)
+            cell["source"] = updated_source
+
+
 def split_off_solutions(srcfilename: str, destfilename: str):
     """
     Load the notebook, adjust headings in markdown cells, and write back.
@@ -60,6 +81,10 @@ def split_off_solutions(srcfilename: str, destfilename: str):
         nb = nbformat.read(inf, as_version=4)
 
     for cell in nb.cells:
+        # rewrite ./attachment/ relative images links as absolute URLs
+        rewrite_attachment_links(cell)
+
+        # split_off_solutions
         if cell.cell_type == 'code' and has_solution(cell):
             cell_source = cell["source"]
             cell_source = cell_source.replace("@titlesolution", "")
@@ -90,4 +115,5 @@ if __name__ == "__main__":
     srcfilename = os.path.join(PROJECT_DIR, "tutorials/src/python_tutorial.ipynb")
     destfilename = os.path.join(PROJECT_DIR, "tutorials/python_tutorial.ipynb")
     print('Processing', srcfilename)
+    clear_solutions_dir()
     split_off_solutions(srcfilename, destfilename)
