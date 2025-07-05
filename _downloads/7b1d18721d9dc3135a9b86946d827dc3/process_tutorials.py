4eac94ef4e574ea06e4983a8ee130e538433d2e9
ivanistheone <ivanistheone@users.noreply.github.com>
2025-07-05 03:00:37 +0000

    deploy: 0c82673d3c34868be047aafe33d1b453c70fcea6

---
+++ _downloads/7b1d18721d9dc3135a9b86946d827dc3/process_tutorials.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+"""
+Process tutorials
+
+Folder structure:
+    tutorials/
+        src/
+            python_tutorial_src.ipynb
+            pandas_tutorial_src.ipynb
+            seaborn_tutorial_src.ipynb
+            attachments/
+                my_figrue.png               image served via raw.gihub 
+        python_tutorial.ipynb
+        pandas_tutorial.ipynb
+        seaborn_tutorial.ipynb
+        solutions/
+            python/
+                Exercise_1_something.py
+        static/
+            94u93u0293u4i0329i409i.png      images served via raw.github ...
+
+
+Processing steps:
+- replace attachment/ image includes with URLs pointing to raw.github..
+- output TEX
+    - replace URLs with local copy
+    - build using IEEEtrans format
+- output HTML static
+    - clean formatting to use as standalone webapge
+
+# TODO: import 
+# def extract_solutions(nb, nb_dir, nb_name):
+# from process_notebooks.py
+"""
+from copy import deepcopy
+import glob
+import nbformat
+import os
+import re
+
+
+PROJECT_DIR = "/Users/ivan/Projects/Minireference/STATSbook/noBSstatsnotebooks/"
+SOLUTIONS_DIR = "tutorials/solutions/python/"
+SOLUTIONS_PATH = os.path.join(PROJECT_DIR, SOLUTIONS_DIR)
+GITHUB_TREE_URL = "https://github.com/minireference/noBSstats/blob/main"
+GITHUB_RAW_TREE_URL = "https://raw.githubusercontent.com/minireference/noBSstats/main"
+
+def has_solution(cell):
+    """Return True if cell is marked as containing an exercise solution."""
+    cell_text = cell["source"].replace(" ", "").lower()
+    first_line = cell_text.split("\n")[0]
+    return (
+        cell_text.startswith("#@titlesolution")
+        or "to_remove" in first_line
+    )
+
+
+def clear_solutions_dir():
+    solutions_dir = SOLUTIONS_PATH
+    solutions_py_pat = os.path.join(solutions_dir, '*.py')
+    solutions_py_files = glob.glob(solutions_py_pat)
+    for solutions_py_file in solutions_py_files:
+        os.remove(solutions_py_file)
+
+
+def rewrite_attachment_links(cell):
+    tut_src_att_url = GITHUB_RAW_TREE_URL + "/tutorials/src/attachments/"
+    if cell.cell_type == 'markdown':
+        cell_source = cell["source"]
+        attachments_pat = r'(?:\./)?attachments/'
+        if re.search(attachments_pat, cell_source):
+            updated_source = re.sub(attachments_pat, tut_src_att_url, cell_source)
+            cell["source"] = updated_source
+
+
+def split_off_solutions(srcfilename: str, destfilename: str):
+    """
+    Load the notebook, adjust headings in markdown cells, and write back.
+    """
+    with open(srcfilename, 'r', encoding='utf-8') as inf:
+        nb = nbformat.read(inf, as_version=4)
+
+    for cell in nb.cells:
+        # rewrite ./attachment/ relative images links as absolute URLs
+        rewrite_attachment_links(cell)
+
+        # split_off_solutions
+        if cell.cell_type == 'code' and has_solution(cell):
+            cell_source = cell["source"]
+            cell_source = cell_source.replace("@titlesolution", "")
+            title = cell_source.splitlines()[0].strip()
+            filename = title.replace("# ","").replace(" ","_") + ".py"
+            with open(os.path.join(SOLUTIONS_PATH, filename), "w") as solf:
+                solf.write(cell_source)
+
+            # clear cell outputs
+            if "outputID" in cell["metadata"]:
+                del cell["metadata"]["outputId"]
+            if "outputs" in cell:
+                del cell["outputs"]
+            if "execution_count" in cell:
+                del cell["execution_count"]
+
+            # set cell content to link to the solution .py
+            cell["cell_type"] = "markdown"
+            py_url = f"{GITHUB_TREE_URL}/tutorials/solutions/python/{filename}"
+            new_source = f'<a href=\"{py_url}\" target=\"_blank\">Click for solution.</a>\n'
+            cell['source'] = new_source
+
+    with open(destfilename, 'w', encoding='utf-8') as outf:
+        nbformat.write(nb, outf)
+
+
+if __name__ == "__main__":
+    srcfilename = os.path.join(PROJECT_DIR, "tutorials/src/python_tutorial_src.ipynb")
+    destfilename = os.path.join(PROJECT_DIR, "tutorials/python_tutorial.ipynb")
+    print('Processing', srcfilename)
+    clear_solutions_dir()
+    split_off_solutions(srcfilename, destfilename)
---
+++ _sources/docs/notebooks/20_PROB.md
@@ -0,0 +1,55 @@
+# Chapter 2 — Probability theory
+
+Probability theory is a language for describing randomness, variability, and uncertainty.
+You need to know probability theory to understand statistics.
+This is why Part 1 of the book includes ~200 pages of formulas,
+explanations, figures, and code examples
+to get you up to speed on all the probability theory topics that you need to know.
+
+Below,
+you'll find the computational notebooks from Chapter 2 of the book.
+Going through these notebooks would be an interesting even if you don't have the book,
+since the notebooks are mostly self contained.
+
+## Notebooks
+
+Each notebook contains the code examples from corresponding section.
+If you're reading the book,
+you can follow along by running the commands in the these notebooks,
+to run all the probability calculations for yourself.
+
+- Discrete random variables [21_discrete_random_vars.ipynb](./21_discrete_random_vars.ipynb)
+- Multiple random variables [22_multiple_random_vars.ipynb](./22_multiple_random_vars.ipynb)
+- Inventory of discrete distributions [23_inventory_discrete_dists.ipynb](./23_inventory_discrete_dists.ipynb)
+- Continuous random variables [24_continuous_random_vars.ipynb](./24_continuous_random_vars.ipynb)
+- Inventory of continuous distributions [25_inventory_continuous_dists.ipynb](./25_inventory_continuous_dists.ipynb)
+- Random variable generation [26_simulations.ipynb](./26_simulations.ipynb)
+- Probability models for random samples [27_random_samples.ipynb](./27_random_samples.ipynb)
+
+
+## Exercises
+
+Each section contains the exercises to help you practice probability calculations
+covered explained in that section.
+
+
+
+
+### Probability models for real world data
+
+Here is a list of the different domains that can be usefully described using probability distributions:
+- math models for r.v. $X$ (defined as probability distribution function $f_X$)
+- computer models like `rvX` created from one of the model families in `scipy.stats`
+  initialized with appropriate parameters.
+  The computer model `rvX` for the random variable $X$ has
+  methods like: `rvX.rvs()`, `rvX.cdf(b)`, `pdf/pmf`,
+  and stats like `rvX.mean()`, `rvX.median()`, `rvX.var()`, `rvX.std()`, `rvQ.ppf(q)`, etc.
+- random draws form a generative process (computer simulation that generates random numbers, see [Section 2.7](./27_simulations.ipynb) for examples)
+- random draws from a real world process (e.g. factory that produces a new item)
+- data for an entire population (census)
+- sample data from a population (the data type we learned about in Chapter 1)
+- synthetic data obtained by resampling:
+  - bootstrap samples = sampling from the empirical distribution
+  - permutation test that forget group membership
+
+
