d8c3f8271e9e745657f579d20f80928b8ce1aa36
ivanistheone <ivanistheone@users.noreply.github.com>
2025-10-16 17:57:43 +0000

    deploy: 117767fa775bbf98e2548340bb0fba8387929422

---
+++ site/notebooks/stats_helpers/stats_helpers.py
@@ -0,0 +1,110 @@
+
+
+
+#######################################################
+# max width that fits in the code blocks is 55 chars  #
+
+# DESCRIPTIVE STATISTICS
+################################################################################
+
+# moved to https://github.com/minireference/ministats/blob/main/ministats/estimators.py
+
+
+
+# ESTIMATORS
+################################################################################
+
+# moved to https://github.com/minireference/ministats/blob/main/ministats/estimators.py
+
+
+
+
+# UTILS
+################################################################################
+
+# moved to https://github.com/minireference/ministats/blob/main/ministats/formulas.py
+
+
+
+# SAMPLING DISTRIBUTIONS
+################################################################################
+
+# moved to https://github.com/minireference/ministats/blob/main/ministats/sampling.py
+
+
+
+# BOOTSTRAP
+################################################################################
+
+# moved to https://github.com/minireference/ministats/blob/main/ministats/sampling.py
+
+
+
+
+
+# CONFIDENCE INTERVALS
+################################################################################
+
+# moved to https://github.com/minireference/ministats/blob/main/ministats/confidence_intervals.py
+
+
+# TAIL CALCULATION UTILS
+################################################################################
+
+# moved to https://github.com/minireference/ministats/blob/main/ministats/hypothesis_tests.py
+
+
+
+# BASIC TESTS (used for kombucha data generation)
+################################################################################
+
+# moved to https://github.com/minireference/ministats/blob/main/ministats/hypothesis_tests.py
+
+
+
+# SIMULATION TESTS (Section 3.3)
+################################################################################
+
+# moved to https://github.com/minireference/ministats/blob/main/ministats/hypothesis_tests.py
+
+
+
+# BOOTSTRAP TEST FOR THE MEAN (cut material)
+################################################################################
+
+# moved to https://github.com/minireference/ministats/blob/main/ministats/hypothesis_tests.py
+
+
+
+# PERMUTATION TEST DMEANS
+################################################################################
+
+# moved to https://github.com/minireference/ministats/blob/main/ministats/hypothesis_tests.py
+
+
+# PERMUTATION ANOVA
+################################################################################
+
+# moved to https://github.com/minireference/ministats/blob/main/ministats/hypothesis_tests.py
+
+
+
+
+# STANDARDIZED EFFECT SIZE MEASURES
+################################################################################
+
+# moved to https://github.com/minireference/ministats/blob/main/ministats/formulas.py
+
+
+# T-TESTS
+################################################################################
+
+# moved to https://github.com/minireference/ministats/blob/main/ministats/hypothesis_tests.py
+
+
+
+# SIMULATION OF CONFIDENCE INTERVAL PROPERTIES
+################################################################################
+
+# moved to https://github.com/minireference/ministats/blob/main/ministats/simulations.py
+
